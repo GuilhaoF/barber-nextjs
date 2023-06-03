056d15abab88e44a16b09a713b7c49aca0a5f9d7
luis <luisphelipe1000@gmail.com>
2023-06-03 13:38:54 -0300

    novo readme

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-## BarberPro ( Front + Back)
+<h1 align="center" id="top">
+  <img alt="icone-barbearia" title="barberia-logo" width="300px"  src="https://github.com/GuilhaoF/barber-nextjs/assets/67026555/1636ba51-256c-440b-a46d-d804056aeaa1" />
+</h1>
+
+# BarberPro
+
+O BarberPro um sistema de barbearia onde posso fazer várias operações como cadastrar o cliente, escolher o corte, finalizar o seu atendimento e ainda adicionar tipos de cortes e atualiza-los. O front-end foi feito em NextJs com Typescript, utilizando todos os recursos disponíveis oferecidos pelo framework como SSR e SSG entre outros e na ui ChakraUI
+
+## Demonstração
+
+Desktop
+
+<p align="center"> 
+ <img src="" alt="app-desktop"/>
+</p>
+
+## Funcionando
+
+<p align="center"> 
+ <div src="https://github.com/GuilhaoF/barber-nextjs/assets/67026555/a91d8d87-db40-4dff-94bb-dafc97c6f9e5"></div>
+</p>
+
+## 🚀 Tecnologias e Bibliotecas Ultilizadas
+
+- [NextJS]()
+- [Typescript]()
+- [Axios]()
+- [ChakraUi]()
+- [Nookies]()
+- [Stript]()
+
+## Clonar o repositório
+
+```bash
+$ git clone https://github.com/GuilhaoF/barber-nextjs.git
+```
+
+## Instalar dependências do Projeto
+
+```bash
+    yarn
+```
+
+## Rodar Projeto
+
+```bash
+    yarn dev
+```
+
+## Env.local
+
+Preencha essas variaveis
+
+```bash
+
+  NEXT_PUBLIC_API_URL
+  NEXT_PUBLIC_STRIPE_PUBLIC_KEY
+
+```
+
+## Feedback
+
+Se você tiver algum feedback, por favor nos deixe saber por meio de luisphelipe1000@gmail.com
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/luis-felipe-silv/)
+
+## :memo: License
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+Made with :heart: by <a href="https://github.com/guilhaof" target="_blank">Luis Felipe G</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- src/pages/haircuts/index.tsx
+++ src/pages/haircuts/index.tsx
@@ -18,7 +18,7 @@ import { IoMdPricetag } from 'react-icons/io'
 import { canSSRAuth } from '../../utils/canSSRAuth'
 import { setupAPIClient } from '../../services/api'
 
-interface HaircutsItem{
+interface HaircutsItem {
   id: string;
   name: string;
   price: number | string;
@@ -26,27 +26,27 @@ interface HaircutsItem{
   user_id: string;
 }
 
-interface HaircutsProps{
+interface HaircutsProps {
   haircuts: HaircutsItem[];
 }
 
 
-export default function Haircuts({ haircuts }: HaircutsProps){
+export default function Haircuts({ haircuts }: HaircutsProps) {
 
   const [isMobile] = useMediaQuery("(max-width: 500px)")
 
   const [haircutList, setHaircutList] = useState<HaircutsItem[]>(haircuts || [])
   const [disableHaircut, setDisableHaircut] = useState("enabled")
 
-  async function handleDisable(e: ChangeEvent<HTMLInputElement>){
+  async function handleDisable(e: ChangeEvent<HTMLInputElement>) {
     const apiClient = setupAPIClient();
-    
-    if(e.target.value === 'disabled'){
+
+    if (e.target.value === 'disabled') {
 
       setDisableHaircut("enabled")
-      
+
       const response = await apiClient.get('/haircuts', {
-        params:{
+        params: {
           status: true
         }
       })
@@ -54,11 +54,11 @@ export default function Haircuts({ haircuts }: HaircutsProps){
       setHaircutList(response.data);
 
 
-    }else{
+    } else {
 
       setDisableHaircut("disabled")
       const response = await apiClient.get('/haircuts', {
-        params:{
+        params: {
           status: false
         }
       })
@@ -70,79 +70,79 @@ export default function Haircuts({ haircuts }: HaircutsProps){
 
   }
 
-  return(
+  return (
     <>
       <Head>
         <title>Modelos de corte - Minha barbearia</title>
       </Head>
       <Sidebar>
         <Flex direction="column" alignItems="flex-start" justifyContent="flex-start">
-         
-         <Flex
-          direction={isMobile ? 'column' : 'row'}
-          w="100%"
-          alignItems={isMobile ? 'flex-start' : 'center'}
-          justifyContent="flex-start"
-          mb={0}
-         >
-          <Heading
-            fontSize={isMobile ? '28px' : "3xl"} 
-            mt={4} 
-            mb={4}
-            mr={4}
-            color="orange.900"
+
+          <Flex
+            direction={isMobile ? 'column' : 'row'}
+            w="100%"
+            alignItems={isMobile ? 'flex-start' : 'center'}
+            justifyContent="flex-start"
+            mb={0}
           >
-            Modelos de corte
-          </Heading>
-
-          <Link href="/haircuts/new">
-            <Button>
-              Cadastrar novo
-            </Button>
-          </Link>
-
-          <Stack ml="auto" align="center" direction="row">
-            <Text fontWeight="bold">ATIVOS</Text>
-            <Switch
-              colorScheme="green"
-              size="lg"
-              value={disableHaircut}
-              onChange={ (e: ChangeEvent<HTMLInputElement>) => handleDisable(e) }
-              isChecked={disableHaircut === 'disabled' ? false : true}
-            />
-          </Stack>
-
-         </Flex>
-
-
-        {haircutList.map(haircut => (
-          <Link key={haircut.id} href={`/haircuts/${haircut.id}`}>
-            <Flex
-              cursor="pointer"
-              w="100%"
-              p={4}
-              bg="barber.400"
-              direction={isMobile ? "column" : "row"}
-              align={isMobile ? "flex-start" : "center"}
-              rounded="4"
-              mb={2}
-              justifyContent="space-between"
+            <Heading
+              fontSize={isMobile ? '28px' : "3xl"}
+              mt={4}
+              mb={4}
+              mr={4}
+              color="orange.900"
             >
-  
-              <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center" >
-                <IoMdPricetag size={28} color="#fba931"/>
-                <Text fontWeight="bold" ml={4} noOfLines={2} color="white">
-                  {haircut.name}
+              Modelos de corte
+            </Heading>
+
+            <Link href="/haircuts/new">
+              <Button>
+                Cadastrar novo
+              </Button>
+            </Link>
+
+            <Stack ml="auto" align="center" direction="row">
+              <Text fontWeight="bold" color="#fff">ATIVOS</Text>
+              <Switch
+                colorScheme="green"
+                size="lg"
+                value={disableHaircut}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleDisable(e)}
+                isChecked={disableHaircut === 'disabled' ? false : true}
+              />
+            </Stack>
+
+          </Flex>
+
+
+          {haircutList.map(haircut => (
+            <Link key={haircut.id} href={`/haircuts/${haircut.id}`}>
+              <Flex
+                cursor="pointer"
+                w="100%"
+                p={4}
+                bg="barber.400"
+                direction={isMobile ? "column" : "row"}
+                align={isMobile ? "flex-start" : "center"}
+                rounded="4"
+                mb={2}
+                justifyContent="space-between"
+              >
+
+                <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center" >
+                  <IoMdPricetag size={28} color="#fba931" />
+                  <Text fontWeight="bold" ml={4} noOfLines={2} color="white">
+                    {haircut.name}
+                  </Text>
+                </Flex>
+
+                <Text fontWeight="bold" color="white">
+                  Preço: R$ {haircut.price}
                 </Text>
+
               </Flex>
-  
-              <Text fontWeight="bold" color="white">
-                Preço: R$ {haircut.price}
-              </Text>
-  
-            </Flex>
-          </Link>
-        ))}
+            </Link>
+          ))}
 
 
         </Flex>
@@ -154,20 +154,20 @@ export default function Haircuts({ haircuts }: HaircutsProps){
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
 
-  try{
+  try {
 
     const apiClient = setupAPIClient(ctx);
     const response = await apiClient.get('/haircuts',
-    {
-      params:{
-        status: true,
-      }
-    })
+      {
+        params: {
+          status: true,
+        }
+      })
 
 
-    if(response.data === null){
-      return{
-        redirect:{
+    if (response.data === null) {
+      return {
+        redirect: {
           destination: '/dashboard',
           permanent: false,
         }
@@ -175,16 +175,16 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
     }
 
 
-    return{
+    return {
       props: {
         haircuts: response.data
       }
     }
 
-  }catch(err){
+  } catch (err) {
     console.log(err);
-    return{
-      redirect:{
+    return {
+      redirect: {
         destination: '/dashboard',
         permanent: false,
       }
